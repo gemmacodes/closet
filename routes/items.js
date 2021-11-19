7fc0e2cff92d1883a8ddc6c8913bf0cfc79711a2
GemmaFornons <gemmafornons@GemmaFornonss-MacBook-Pro.local>
2021-11-19 17:44:00 +0100

    GET method (filtering) SEEMS to work

--- routes/items.js
+++ routes/items.js
@@ -2,36 +2,82 @@ var express = require("express");
 var router = express.Router();
 const db = require("../model/helper");
 var models = require("../models");
+const { Sequelize } = require('sequelize');
 
-// NEEDS FIXING: gets items from a given category AND color AND season
-router.get("/", async function (req, res) {
 
-  // const { categories, colors, seasons } = req.query;
-
-  // if (categories) const categoriesJoined = categories.join(",")
-  // else categoriesJoined = categories; 
-  // if (seasons) const seasonsJoined = seasons.join(",")
-  // else seasonsJoined = seasons; 
-  // if (colors) const colorsJoined = colors.join(",")
-  // else colorsJoined = colors; 
-
+// WORKS: gets all items with color AND season
+router.get("/all", async function (req, res) {
 
   try {
-    const { categories } = req.query;
-    const { colors } = req.query;
-    const { seasons } = req.query;
+    const items = await models.Item.findAll({
+      include: 
+      [
+        models.Season, 
+        models.Color
+      ]
+    });
+
+    res.send(items);
     
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// WORKS? gets items from a given category AND color AND season
+router.get("/", async function (req, res) {
+
+  try {
+    const { categories, colors, seasons } = req.query;
+    let selectedCategories = [];
+    let selectedSeasons = [];
+    let selectedColors = [];
+
+// if categories / seasons / colors are not in the query (no related checkboxes were selected), assumes all checkboxes were selected
+    
+    if (!categories){
+      const categoryList = await models.Category.findAll(
+        {
+          attributes: ['id']
+        }
+      );
+      selectedCategories = categoryList.map(e => e.dataValues.id);  // turns categoryList into an array of values [1,2,3,4,5....]
+
+    } else {selectedCategories = categories.split(",")};
+
+
+    if (!seasons) { 
+      const seasonList = await models.Season.findAll(
+        {
+          attributes: ['id']
+        }
+      );
+      selectedSeasons = seasonList.map(e => e.dataValues.id);  // turns seasonList into an array of values [1,2,3,4,5....]
+
+    } else {selectedSeasons = seasons.split(",")};
+
+    if (!colors) { 
+      const seasonList = await models.Color.findAll(
+        {
+          attributes: ['id']
+        }
+      );
+      selectedColors = colorList.map(e => e.dataValues.id);  // turns seasonList into an array of values [1,2,3,4,5....]
+
+    } else {selectedColors = colors.split(",")};
+
+// finds all items matching color, season and category filters
 
     const items = await models.Item.findAll({
       where: {
-        CategoryId: categories,
+        CategoryId: selectedCategories  // this property is in the Items table
       },
       include: [
       {
         model: models.Season,
         through:{
           where: { 
-            SeasonId: seasons
+            SeasonId: selectedSeasons // this property is not in the Items table
           }
         },
         required: true
@@ -40,8 +86,8 @@ router.get("/", async function (req, res) {
         model: models.Color,
         through:{
           where: { 
-            ColorId: colors
-          }
+            ColorId: selectedColors // this property is not in the Items table
+          },
         },
         required: true 
       }
@@ -55,78 +101,6 @@ router.get("/", async function (req, res) {
   }
 });
 
-// // NEEDS FIXING: gets items from a given category AND color AND season
-// router.get("/items", async function (req, res) {
-//   try {
-//     const { categories } = req.query;
-//     const { colors } = req.query;
-//     const { seasons } = req.query;
-
-//     let results = null;
-
-//     // if no filters are selected, all items are returned
-//     if (
-//       (!categories || !categories.length) &&
-//       (!colors || !colors.length) &&
-//       (!seasons || !seasons.length)
-//     ) {
-//       results = await db("SELECT * FROM items ORDER BY id ASC;");  
-
-//     // if all filters are selected, items are selected according to those
-//     } else if (categories && colors && seasons) {
-//       const categoriesJoined = categories.join(",");
-//       const colorsJoined = colors.join(",");
-//       const seasonsJoined = seasons.join(",");
-//       results = await db(
-//         `SELECT * FROM items WHERE (category_id) IN (${categoriesJoined}) AND (color_id) IN (${colorsJoined}) AND (season_id) IN (${seasonsJoined});`
-//       );
-//     } else if (categories && colors) {
-//       const categoriesJoined = categories.join(",");
-//       const colorsJoined = colors.join(",");
-//       results = await db(
-//         `SELECT * FROM items WHERE (category_id) IN (${categoriesJoined}) AND (color_id) IN (${colorsJoined});`
-//       );
-//     } else if (categories && seasons) {
-//       const categoriesJoined = categories.join(",");
-//       const seasonsJoined = seasons.join(",");
-//       results = await db(
-//         `SELECT * FROM items WHERE (category_id) IN (${categoriesJoined}) AND (season_id) IN (${seasonsJoined});`
-//       );
-//     } else if (colors && seasons) {
-//       const colorsJoined = colors.join(",");
-//       const seasonsJoined = seasons.join(",");
-//       results = await db(
-//         `SELECT * FROM items WHERE (color_id) IN (${colorsJoined}) AND (season_id) IN (${seasonsJoined});`
-//       );
-//     } else if ((!colors || !colors.length) && (!seasons || !seasons.length)) {
-//       const categoriesJoined = categories.join(",");
-//       results = await db(
-//         `SELECT * FROM items WHERE (category_id) IN (${categoriesJoined});`
-//       );
-//     } else if (
-//       (!categories || !categories.length) &&
-//       (!seasons || !seasons.length)
-//     ) {
-//       const colorsJoined = colors.join(",");
-//       results = await db(
-//         `SELECT * FROM items WHERE (color_id) IN (${colorsJoined});`
-//       );
-//     } else if (
-//       (!categories || !categories.length) &&
-//       (!colors || !colors.length)
-//     ) {
-//       const seasonsJoined = seasons.join(",");
-//       results = await db(
-//         `SELECT * FROM items WHERE (season_id) IN (${seasonsJoined});`
-//       );
-//     }
-
-//     res.send(results.data);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send(error);
-//   }
-// });
 
 // WORKS! adds new items to items table on DB; returns new item
 router.post("/", async function (req, res) {
@@ -148,13 +122,13 @@ router.delete("/:id", async function (req, res) {
   try {
       const { id } = req.params;
 
-      const item = await models.Item.destroy({
+      await models.Item.destroy({
         where: {
           id,
         },
       });
-  
-      res.send("Hello");
+    
+      res.send("Deleted!");
   
     } catch (error) {
       res.status(500).send(error);
